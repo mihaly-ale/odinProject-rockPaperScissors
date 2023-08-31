d2d3cc77d048b982cb68f0ffe9a5a3a03a2c083c
mihaly-ale <105678319+mihaly-ale@users.github.com>
2023-09-01 00:29:30 +0200

    Fixed variable scopes in playRound function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
 
     let array = ["rock", "paper", "scissors"]
     let index = Math.floor(Math.random() * 3);
@@ -7,51 +7,52 @@ function getComputerChoice () {
 }
 const computerSelection = getComputerChoice();
 
-console.log(`computer selection: ${computerSelection}`);
 
 
-function getPlayerChoice () {
+function getPlayerChoice() {
     let input = prompt("Which do you choose: Rock, Paper, Scissors?", 0)
-    
+
     switch (input) {
         case "rock":
         case "Rock":
         case "ROCK":
-            return ("Player selected rock!");
+            // console.log("Player selected rock!");
+            return "rock";
             break;
         case "paper":
         case "Paper":
         case "PAPER":
-            return ("Player selected paper!");
+            // console.log("Player selected paper!");
+            return "paper";
             break;
         case "scissors":
         case "Scissors":
         case "SCISSORS":
-            return ("Player selected scissors!");
+            // console.log("Player selected scissors!");
+            return "scissors";
             break;
         default:
-                alert("Check your input!");
-                break;        
+            alert("Check your input!");
+            break;
     }
-}  
+}
 
 const playerSelection = getPlayerChoice();
-        
+
+
+
 console.log(`player selection: ${playerSelection}`);
+console.log(`computer selection: ${computerSelection}`);
 
+function playRound() {
 
-
-function playRound(playerSelection, computerSelection) {
-
-    console.log(playerSelection)
-    
-    if(playerSelection == "rock" && computerSelection == "scissors") {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
         console.log("You won! Rock beats scissors.")
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         console.log("You lost! Paper beats rock.")
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         console.log("You won! Paper beats rock.")
-    } else if(playerSelection == "paper" && computerSelection == "scissors") {
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
         console.log("You lost! Scissors beat paper")
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         console.log("You lost! Rock beats scissors")
