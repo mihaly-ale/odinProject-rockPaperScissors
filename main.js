c8a63171ac2e4d8bc962e18c2387bf8af9d2fe43
mihaly-ale <105678319+mihaly-ale@users.github.com>
2023-09-21 00:03:25 +0200

    Add modal to display end of game
    1. Add dialog to html
    2. Add to JS closeModal()
    3. Manage different way modal can be closed(reset button, escape, and close button)
    4. Style dialog element in css

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const buttons = document.querySelectorAll('input[id^="btn"]');
-const resultMsg = document.getElementById("resultMessage")
-const playerScoreField = document.getElementById("playerScoreField")
-const computerScoreField = document.getElementById("computerScoreField")
-const resetButton = document.getElementById("reset")
+const buttons = document.querySelectorAll("input[id^='btn']");
+const resultMsg = document.getElementById("resultMessage");
+const playerScoreField = document.getElementById("playerScoreField");
+const computerScoreField = document.getElementById("computerScoreField");
+const resetBtn = document.getElementById("reset-button");
 let playerScore = 0;
 let computerScore = 0;
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-button");
+
+buttons.forEach(button => button.addEventListener("click", handleClick));
+resetBtn.addEventListener("click", resetGame);
+closeBtn.addEventListener("click", closeModal);
 
 
-buttons.forEach(button => button.addEventListener('click', handleClick))
-resetButton.addEventListener('click', resetGame);
 
 function handleClick(e) {
-    console.log(e.target.dataset.name)
     let playerSelection = e.target.dataset.name;
     const computerSelection = getComputerChoice();
     playGame(playerSelection, computerSelection);
@@ -19,13 +22,12 @@ function handleClick(e) {
 }
 
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"]
+    let choices = ["rock", "paper", "scissors"];
     let randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
 function playGame(playerSelection, computerSelection) {
-
 
     if (playerSelection === computerSelection) {
         resultMsg.textContent = `Computer selected ${computerSelection}.It's a draw.`;
@@ -41,23 +43,28 @@ function playGame(playerSelection, computerSelection) {
         computerScore++;
         computerScoreField.textContent = computerScore;
         resultMsg.textContent = `You lost. ${computerSelection.charAt(0).toUpperCase()}${computerSelection.slice(1)} beats ${playerSelection}.`
-    }
-
-   
+    }   
 }
 
 function isWinner() {
 
     if (playerScore === 5) {
-        resultMsg.textContent = "Well done mate!";
-        buttons.forEach(button => button.disabled = true);
-        resetButton.style.display = "inline-block";   
+            if(computerScore === 4 || computerScore === 3) {
+                endMessage.textContent = "Almost got ya!";
+            } else {
+                endMessage.textContent = "Woah! Smooth one.";
+            }
+        modal.showModal();
+        buttons.forEach(button => button.disabled = true); 
     } else if (computerScore === 5) {
-        resultMsg.textContent = "Better play an easier game? Huh!?";
+        if (playerScore === 4 || playerScore === 3){
+            endMessage.textContent = "You can't catch me!";
+        } else {
+            endMessage.textContent = "Yo dawg! You just got busted.";
+        }
+        modal.showModal();      
         buttons.forEach(button =>button.disabled = true) ;
-        resetButton.style.display = "inline-block";  
     }
-
 }
 
 function resetGame() {
@@ -67,10 +74,28 @@ function resetGame() {
         playerScoreField.innerHTML = playerScore;
         computerScoreField.innerHTML = computerScore;
         buttons.forEach(button => button.disabled = false)
-        resetButton.style.display = "none";
-
+        resultMsg.textContent = "";
+        modal.close();
     }
 }
+
+function closeModal(e) {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreField.innerHTML = playerScore;
+    computerScoreField.innerHTML = computerScore;
+    buttons.forEach(button => button.disabled = false)
+    resultMsg.textContent = "";
+    modal.close();
+}
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && modal.hasAttribute('open')) {
+      closeModal();
+    }
+  });
+
+
+
 
 
 
